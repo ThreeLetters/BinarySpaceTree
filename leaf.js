cb79fa6fce12dcb782a66b5637c0c05ce234582e
Andrew S <andrews54757@gmail.com>
2017-05-30 08:32:11 -0400

    Update leaf.js

--- leaf.js
+++ leaf.js
@@ -7,7 +7,14 @@ var Leafs = {
       this.BOTTOM;
       this.OBJ = [];
     }
-    
+    getChild(minx,miny,maxx,maxy) {
+      if (miny > this.Y) return this.BOTTOM;
+      
+      if (maxy < this.Y) return this.TOP;
+      
+      return -1;
+      
+    }
     
   },
   horizontal: class LeafHor {
@@ -18,7 +25,14 @@ var Leafs = {
       this.RIGHT;
       this.OBJ = [];
     }
-    
+     getChild(minx,miny,maxx,maxy) {
+      if (minx > this.X) return this.RIGHT;
+      
+      if (maxx < this.X) return this.LEFT;
+      
+      return -1;
+      
+    }
     
     
   }
