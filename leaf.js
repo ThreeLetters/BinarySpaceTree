3356b2b3baeafce1c2326a869426ea321a904faf
Andrew S <andrews54757@gmail.com>
2017-05-30 08:24:24 -0400

    Update leaf.js

--- leaf.js
+++ leaf.js
@@ -1,1 +1,19 @@
-
+var Leafs = {
+  vertical: class LeafVert {
+    constructor(y) {
+     this.isVert = 1;
+     this.Y = y; 
+    }
+    
+    
+  },
+  horizontal: class LeafHor {
+    constructor(x) {
+      this.isVert = 0;
+      this.X = x;
+    }
+    
+    
+    
+  }
+}
