7842d8db4ba391a9dc274a4439cd9efa57b08a17
Andrew S <andrews54757@gmail.com>
2017-05-31 09:13:47 -0400

    Update BinarySpaceTree.js

--- browser/BinarySpaceTree.js
+++ browser/BinarySpaceTree.js
@@ -1,21 +1,19 @@
-"use strict"
+"use strict";
 /*
 BinarySpaceTree - Simple Binary Space Tree in JavaScript
     Copyright (C) 2017 Andrew S
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 var MAX = 20;
 var MAXOBJ = 4;
 
@@ -46,9 +44,43 @@ var insert = function (obj, leaf) {
     return insert(obj, insertTo);
 }
 
+var forEachPrecise = function (bounds, leaf, call) {
+    if (!checkIntersect(bounds, leaf.bounds)) {
+        return;
+    }
+    leaf.OBJ.forEach(call);
+    if (leaf.LEFT) {
+        forEachPrecise(bounds, leaf.LEFT, call);
+        forEachPrecise(bounds, leaf.RIGHT, call);
+    } else if (leaf.TOP) {
+        forEachPrecise(bounds, leaf.TOP, call);
+        forEachPrecise(bounds, leaf.BOTTOM, call);
+    }
+
+
+}
+var everyPrecise = function (bounds, leaf, call) {
+    if (!checkIntersect(bounds, leaf.bounds)) {
+        return;
+    }
+    if (!leaf.OBJ.every(call)) return false;
+    if (leaf.LEFT) {
+        if (!everyPrecise(bounds, leaf.LEFT, call)) return false;
+        return everyPrecise(bounds, leaf.RIGHT, call)
+    } else if (leaf.TOP) {
+        if (!everyPrecise(bounds, leaf.TOP, call)) return false;
+        return everyPrecise(bounds, leaf.BOTTOM, call);
+    }
+    return true;
+
+}
+var checkIntersect = function (bounds, bounds2) {
+    return !(bounds2.minX > bounds.maxX || bounds2.maxX < bounds.minX || bounds2.minY > bounds.maxY || bounds2.maxY < bounds.minX)
+}
+
 var Leafs = {
     horizontal: class LeafHor {
-        constructor(x, y, width, height, lvl) {
+        constructor(x, y, width, height, lvl, parent) {
             this.isVert = 0;
             this.LVL = lvl;
             this.WIDTH = width;
@@ -56,10 +88,19 @@ var Leafs = {
             this.X = x;
             this.Y = y;
             this.DIV = y + (height >> 1);
+            this.PARENT = parent;
             this.TOP;
             this.BOTTOM;
             this.OBJ = [];
             this.insert = insert;
+            this.forEachPrecise = forEachPrecise;
+            this.bounds = {
+                minX: x,
+                minY: y,
+                maxX: x + width,
+                maxY: y + height
+            }
+            this.everyPrecise = everyPrecise;
         }
         getChild(minx, miny, maxx, maxy) {
             if (miny > this.DIV) return this.BOTTOM;
@@ -70,34 +111,20 @@ var Leafs = {
 
         }
         split() {
-            this.TOP = new Leafs.vertical(this.X, this.Y, this.WIDTH, this.HEIGHT >> 1, this.LVL + 1);
-            this.BOTTOM = new Leafs.vertical(this.X, this.DIV, this.WIDTH, this.HEIGHT >> 1, this.LVL + 1);
+            this.TOP = new Leafs.vertical(this.X, this.Y, this.WIDTH, this.HEIGHT >> 1, this.LVL + 1, this);
+            this.BOTTOM = new Leafs.vertical(this.X, this.DIV, this.WIDTH, this.HEIGHT >> 1, this.LVL + 1, this);
+            var len = this.OBJ.length
             this.OBJ.forEach((obj) => {
                 insert(obj, this);
             })
+            this.OBJ = this.OBJ.slice(len)
 
 
-        }
-        forEach(call) {
-            this.OBJ.forEach(call);
-            if (this.TOP) {
-                this.TOP.forEach(call);
-                this.BOTTOM.forEach(call);
-            }
-        }
-        every(call) {
-            if (!this.OBJ.every(call)) return false;
-            if (this.TOP) {
-                if (!this.TOP.every(call)) return false;
-                return this.BOTTOM.every(call);
-            } else {
-                return true;
-            }
         }
 
     },
     vertical: class LeafVert {
-        constructor(x, y, width, height, lvl) {
+        constructor(x, y, width, height, lvl, parent) {
             this.isVert = 1;
             this.LVL = lvl;
             this.WIDTH = width;
@@ -105,10 +132,19 @@ var Leafs = {
             this.X = x;
             this.Y = y;
             this.DIV = x + (width >> 1);
+            this.PARENT = parent;
             this.LEFT;
             this.RIGHT;
             this.OBJ = [];
             this.insert = insert;
+            this.forEachPrecise = forEachPrecise;
+            this.bounds = {
+                minX: x,
+                minY: y,
+                maxX: x + width,
+                maxY: y + height
+            }
+            this.everyPrecise = everyPrecise;
         }
         getChild(minx, miny, maxx, maxy) {
             if (minx > this.DIV) return this.RIGHT;
@@ -119,57 +155,72 @@ var Leafs = {
 
         }
         split() {
-            this.LEFT = new Leafs.horizontal(this.X, this.Y, this.WIDTH >> 1, this.HEIGHT, this.LVL + 1);
-            this.RIGHT = new Leafs.horizontal(this.DIV, this.Y, this.WIDTH >> 1, this.HEIGHT, this.LVL + 1);
+            this.LEFT = new Leafs.horizontal(this.X, this.Y, this.WIDTH >> 1, this.HEIGHT, this.LVL + 1, this);
+            this.RIGHT = new Leafs.horizontal(this.DIV, this.Y, this.WIDTH >> 1, this.HEIGHT, this.LVL + 1, this);
+            var len = this.OBJ.length;
             this.OBJ.forEach((obj) => {
                 insert(obj, this);
             })
+            this.OBJ = this.OBJ.slice(len)
         }
-        forEach(call) {
-            this.OBJ.forEach(call);
-            if (this.LEFT) {
-                this.LEFT.forEach(call);
-                this.RIGHT.forEach(call);
-            }
+
+
+    }
+}
+class Tree {
+        constructor(width, height, maxObj, maxLvL) {
+            this.WIDTH = width;
+            this.HEIGHT = height;
+            this.MAXOBJ = maxObj || 4;
+            this.MAXLVL = maxLvL || 20;
+            this.ROOT = new Leafs.vertical(0, 0, width, height, 0)
         }
-        every(call) {
-            if (!this.OBJ.every(call)) return false;
-            if (this.LEFT) {
-                if (!this.LEFT.every(call)) return false;
-                return this.RIGHT.every(call);
-            } else {
-                return true;
+        insert(obj) {
+            if (obj._LEAF) {
+                throw "ERR: OBJ already in a LEAF.";
             }
+            return this.ROOT.insert(obj, this.ROOT);
+        }
+        delete(obj) {
+            if (!obj._LEAF) throw "ERR: OBJ is not in a LEAF";
+            var index = obj._LEAF.OBJ.indexOf(obj);
+            obj._LEAF.OBJ.splice(index, 1);
+            obj._LEAF = null;
+        }
+        forEach(bounds, call) {
+            var current = this.ROOT;
+            var minx = bounds.minX,
+                miny = bounds.minY,
+                maxx = bounds.maxX,
+                maxy = bounds.maxY;
+            while (true) {
+                var child = current.getChild(minx, miny, maxx, maxy);
+                if (child == -1 || !child) {
+                    return current.forEach(call);
+                } else {
+                    child.OBJ.forEach(call);
+                    current = child;
+                }
+            }
+
+        }
+        every(bounds, call) {
+            var current = this.ROOT;
+            var minx = bounds.minX,
+                miny = bounds.minY,
+                maxx = bounds.maxX,
+                maxy = bounds.maxY;
+            while (true) {
+                var child = current.getChild(minx, miny, maxx, maxy);
+                if (child == -1 || !child) {
+                    return current.every(call);
+                } else {
+                    if (!child.OBJ.every(call)) return false;
+                    current = child;
+                }
+            }
+
         }
         forEachPrecise(bounds, call) {
-            this.ROOT.forEachPrecise(bounds, this.ROOT, call)
+            this.ROOT.forEachPrecise(bounds, call)
         }
-        everyPrecise(bounds, call) {
-            return this.ROOT.everyPrecise(bounds, this.ROOT, call)
-        }
-
-
-    }
-}
-
-class Tree {
-    constructor(width, height, maxObj, maxLvL) {
-        this.WIDTH = width;
-        this.HEIGHT = height;
-        this.MAXOBJ = maxObj || 4;
-        this.MAXLVL = maxLvL || 20;
-        this.ROOT = new Leafs.vertical(0, 0, width, height, 0)
-    }
-    insert(obj) {
-        if (obj._LEAF) {
-            throw "ERR: OBJ already in a LEAF.";
-        }
-        return insert(obj, this.ROOT);
-    }
-    delete(obj) {
-        if (!obj._LEAF) throw "ERR: OBJ is not in a LEAF";
-        var index = obj._LEAF.OBJ.indexOf(obj);
-        obj._LEAF.OBJ.splice(index, 1);
-        obj._LEAF = null;
-    }
-}
